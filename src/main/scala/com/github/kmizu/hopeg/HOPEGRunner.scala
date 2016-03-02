1aadc5585490a13e6eb7cdbf60547eea1b424052
Kota Mizushima <mizukota@gmail.com>
2016-03-03 00:11:19 +0900

    Arithmetic HOPEG example (thanks @cocoaruto).

--- src/main/scala/com/github/kmizu/hopeg/HOPEGRunner.scala
+++ src/main/scala/com/github/kmizu/hopeg/HOPEGRunner.scala
@@ -20,11 +20,54 @@ object HOPEGRunner {
     )
 
     tryGrammar(
-      "S = F((x -> x x x)) .!; F(f) = f(X);",
+      "u rule",
       HOPEGParser.parse("""
-     |S = F((x -> x x x)); F(f) = f("X");
-      """.stripMargin), "XXX", "YYY", "ZZZ"
+     |S = u((x -> x), "x"); u(f, x) = f(!x .) (u(f, x))?;
+      """.stripMargin), "yy", "x"
     )
+    tryGrammar(
+       "arithmetic",
+      HOPEGParser.parse(
+       """
+      |S = Plus0("");
+      |// '1'を並べた数で自然数を表すとする。
+      |// a+b=c$をチェックする。
+      |Plus0(Left) = Plus1(Left, "") / &(Left "1") Plus0(Left "1");
+      |
+      |Plus1(Left, Right)
+      |  = &(Left "+" Right "=") Plus2(Left, Right)
+      |  / &(Left "+" Right "1") Plus1(Left, Right "1");
+      |
+      |Plus2(Left, Right)
+      |  = Left "+" Right "=" Left Right "$";
+      |
+      |// a*b=c$をチェックする。
+      |Mul0(Left)
+      |  = &(Left "*") Mul1(Left, "", "")
+      |  / &(Left "1") Mul0(Left "1");
+      |
+      |Mul1(Left, Right, Prod)
+      |  = &(Left "+" Right "=") Mul2(Left, Right, Prod)
+      |  / &(Left "+" Right "1") Mul1(Left, Right "1", Prod Left);
+      |
+      |Mul2(Left, Right, Prod)
+      |  = Left "+" Right "=" Prod "$";
+      |
+      |
+      |// a-b=c$をチェックする。
+      |Minus0(Right)
+      |  = &(.* "-" Right "=") Minus1(Right, "")
+      |  / &(.* "-" Right "1") Minus0(Right "1");
+      |
+      |Minus1(Right, Diff)
+      |  = &(.* "-" Right "=" Diff "$") Minus2(Right, Diff)
+      |  / &(.* "-" Right "=" Diff "1") Minus1(Right, Diff "1");
+      |
+      |Minus2(Right, Diff)
+      |  = Right Diff "-" Right "=" Diff "$";
+      |
+      |// a/b=cも同様にチェックできるはず。
+      """.stripMargin), "1+1=11$", "111+11=11111$", "111+1=11111$")
   }
 
   def tryGrammar(name: String, grammar: Ast.Grammar, inputs: String*): Unit = {
