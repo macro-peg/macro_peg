e3848602a0868c3d16f94be058c42bcfd84cab46
Kota Mizushima <kmizu.main@gmail.com>
2025-05-19 20:25:57 +0900

    Add test for lambda expression

---
+++ src/test/scala/com/github/kmizu/macro_peg/LambdaEvalSpec.scala
@@ -0,0 +1,23 @@
+package com.github.kmizu.macro_peg
+
+import com.github.kmizu.macro_peg.EvaluationResult.{Success, Failure}
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.diagrams.Diagrams
+
+class LambdaEvalSpec extends AnyFunSpec with Diagrams {
+  describe("Lambda expression evaluation") {
+    it("applies lambda as macro argument") {
+      val grammar = Parser.parse(
+        """S = Double((x -> x x), "aa") !.;
+          |Double(f: ?, s: ?) = f(f(s));
+        """.stripMargin)
+      val expanded = MacroExpander.expandGrammar(grammar)
+      val evaluator = Evaluator(expanded)
+      val resultSuccess = evaluator.evaluate("aaaaaaaa", Symbol("S"))
+      val resultFailure = evaluator.evaluate("aaaa", Symbol("S"))
+      assert(resultSuccess == Success(""))
+      assert(resultFailure == Failure)
+    }
+  }
+}
+
